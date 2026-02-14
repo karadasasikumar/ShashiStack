97ecadbd36f699df913b6094dfcc698c3561ed06
karada sasikumar <karadasasikumar@gmailcom>
2026-02-14 20:55:39 +0530

    map, filter,reduce

---
+++ Python/Core/high order function/function.py
@@ -0,0 +1,38 @@
+#5th question
+# k=list(map(ord, input()))  #ACII VALUE
+# print(k)
+
+#6TH QUESTION
+# v="AEIOUaeiou"
+# st=list(filter(lambda x: x not in v,input()))
+# print(st)
+
+#from functools import reduce
+
+#7th question
+# l= ['P', 'y', 't', 'h', 'o', 'n']
+# st= reduce(lambda x,y: x+y,l)
+# print(st)
+
+#8th question
+
+# l=[10, 350, 18, 28, 350]
+# k=list(map(lambda x: id(x),l))
+# print(k)
+
+#9th question
+# a=[1,2,3,4]
+# b=[10,20,30,40]
+# k=list(map(lambda x,y: x+y,a,b))
+# print(k)
+
+#10th question
+# nums=[12,15,7,18,20,21,25]
+# l=list(filter(lambda x: (x%3==0) ^ (x%5==0), nums))
+# print(l)
+
+#11th question
+# nums=[[1,2], [3,4],[5,6]]
+# result= list(map(lambda x:x.append(10), nums))
+# print(result,nums,sep='\n')
+
--- Python/Core/high order function/lambda.py
+++ Python/Core/high order function/lambda.py
@@ -1,7 +1,10 @@
+
+#1st question
 # l=[[1,2], [3,4], [5,6]]
 #
 # k=list(map(lambda x: x+[5], l))
 # print(k)
+#from functools import reduce
 
 # d={"apple":100, "banana":40, "cherry":150}
 # print(d.keys())
@@ -27,3 +30,39 @@
 #
 # v=reduce(lambda x,y: x+y, filter(lambda x: x% 5 == 0, map(lambda x:x+2,l)))
 # pprint(v)
+
+#from functiontool import tool
+#7th question
+
+
+#3rd question
+#from functiontools import reduce
+
+# n=(1,10,2)
+#
+# for i in range(1,10,2):
+#     print(i)
+#     if i%5==0:
+#        print("force full termination")
+#        break
+#     print("hi")
+# else:
+#     print("welcome else")
+# print("code terminated")
+
+
+#n=3692
+# while n>0:
+#     r=n%10
+#     r%3n==0
+#     print(r)
+#     break
+# print(r*2)
+# n=n//10
+# print(n)
+
+#4 question
+
+#k=reduce(lambda x,y: x+y, [1,2,3])
+
+
