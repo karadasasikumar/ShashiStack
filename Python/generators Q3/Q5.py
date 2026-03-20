e4f4a646234d62d532dd194ef81254e761d889ce
karada sasikumar <karadasasikumar@gmailcom>
2026-03-20 12:43:41 +0530

    iterator.py

--- Python/generators Q3/Q5.py
+++ Python/generators Q3/Q5.py
@@ -2,11 +2,17 @@
 
 class Iterator:
     def __init__(self,sentence):
-        self.sentence=sentence
+        self.sentence=sentence.split()
         self.index=0
     def __iter__(self):
         return self
     def __next__(self):
         if self.index>=len(self.sentence):
             raise StopIteration
-        sentence=self.sentence(self.index)
+        sentence=self.sentence[self.index]
+        self.index +=1
+        return sentence
+
+s="i love shashi"
+for x in Iterator(s):
+    print(x)
